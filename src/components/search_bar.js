6876dbc65d5f8c2000c108eb4b80256096266c48
Edward Apostol <ed@edapostol.com>
2018-01-31 00:57:26 -0500

    #009: updated state of search component to both set and read state values
    updated search component to use setState and read the state value (specificall this.state.term)

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
 
-const SearchBarDOM = () => {
-	// recall creates React.CreateElement("input") but JSX is supported
-	// through importing te React package
-	let onInputChange = (event) => {
-		console.log(event.target.value);
-	};
-	return <input onChange={onInputChange} />
-};
-
 class SearchBar extends Component {
+
+	SearchBarDOM = () => {
+		// recall creates React.CreateElement("input") but JSX is supported
+		// through importing te React package
+		let onInputChange = (event) => {
+			// console.log(this); // this is SearchBar class
+			// console.log(event.target.value);
+			this.setState( {term: event.target.value } );
+		};
+		// Value of Input: {this.state.term} can confirm state change
+		return (
+			<div>
+			<input value={this.state.term} onChange={onInputChange} />
+			</div>
+		);
+	};
 
 	constructor(props) {
 		super(props);
@@ -17,7 +24,7 @@ class SearchBar extends Component {
 	}
 
 	render() {
-		return (SearchBarDOM());
+		return (this.SearchBarDOM());
 	}
 }
 
