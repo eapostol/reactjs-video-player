f9fc077864298aa962daf643b12a92d7e796417a
Edward Apostol <ed@edapostol.com>
2018-01-31 00:44:44 -0500

    #008: refactored search component, added state and event handling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
+const SearchBarDOM = () => {
 	// recall creates React.CreateElement("input") but JSX is supported
 	// through importing te React package
-	return <input />
+	let onInputChange = (event) => {
+		console.log(event.target.value);
+	};
+	return <input onChange={onInputChange} />
 };
+
+class SearchBar extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { term: ''};
+	}
+
+	render() {
+		return (SearchBarDOM());
+	}
+}
 
 export default SearchBar;
