d039e060fd2b71c7eebffcb0cc52c849fbd43704
Edward Apostol <ed@edapostol.com>
2018-01-30 22:45:00 -0500

    #003L source file for app
    made changes from original webstorm boilerplate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'src/App';
-import 'src/index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+// import App from 'src/App';
+import App from './components/app';
+import reducers from './reducers';
+// import '../style/style.css';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<App />
+	</Provider>
+	, document.querySelector('.container'));
