85edd65dde8f9d4caa0edcbb636c445c682d2923
Edward Apostol <ed@edapostol.com>
2018-01-31 01:14:28 -0500

    #010: retrieved initial youtube search results
    confirmed receipt of youtube search result data using youtube-earch-api library. implemented downward data-flow to ensure that top-level container passes data down to the children appropriately.

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+
+// add youtube api search library
+// npm install --save youtube-api-search
+import YouTubeSearch from 'youtube-api-search';
+
 // ensure directory ref for custom comp
 import SearchBar from './search_bar';
 
 const API_KEY = 'AIzaSyBvvDH0KWcCqgWUZL5bLLYJ75mReUdzrVk';
-// npm install --save youtube-api-search
+
 
 export default class App extends Component {
+	
+	constructor(props){
+		super(props);
+		YouTubeSearch({key:API_KEY,term:'Tron Legacy'},this.onSearchResults.bind(this));
+	}
+
+	onSearchResults(data) {
+	// onSearchResults(videos) {
+		// console.log(data);
+		// console.log(videos);
+		this.setState({videos: data});
+		// this.setState({videos});
+		console.log(this.state.videos);
+	};
+
   render() {
     return (
       <div>
